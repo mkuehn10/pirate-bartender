62c30131d8987783b2be4f395686fc5c1a2016b4
Michael Kuehn <mkuehn10@gmail.com>
2016-07-25 19:02:57 -0400

    set up basic objects and data

---
+++ app/scripts/data.js
@@ -0,0 +1,7 @@
+var questionsData = [{
+    questionText: 'Question 1',
+    correspondingIngredient: 'strong'
+}, {
+    questionText: 'Question 2',
+    correspondingIngredient: 'salty'
+}];
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,51 @@
-console.log('\'Allo \'Allo!');
+$(function () {
+
+    /* Questions constructor */
+    var Question = function(questionText, correspondingIngredient) {
+        var self = this;
+
+        self.questionText = questionText;
+        self.correspondingIngredient = correspondingIngredient;
+    };
+
+    /* Ingredient constructor */
+    var Ingredient = function(typeOfIngredient, listOfIngredients) {
+        var self = this;
+
+        self.typeOfIngredient = typeOfIngredient;
+        self.listOfIngredients = listofIngredients;
+    };
+
+    /* Pantry constructor */
+    var Pantry = function() {
+        var self = this;
+    };
+
+    /* Bartender constructor */
+    var Bartender = function() {
+        var self = this;
+
+        self.createDrink = function() {
+
+        };
+    };
+
+    var BarViewModel = function() {
+        var self = this;
+
+        self.questions = [];
+        self.ingredients = [];
+
+        self.initializeQuestions = function(questions) {
+            ko.utils.arrayForEach(questions, function(question) {
+                self.questions.push(new Question(question.questionText, question.correspondingIngredient));
+            });
+        };
+
+        self.initializeQuestions(questionsData);
+
+
+    };
+
+    ko.applyBindings(new BarViewModel());
+});
