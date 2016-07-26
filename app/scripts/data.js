0fefac91e52524e84d63dc7140f82af3f65ac540
Michael Kuehn <mkuehn10@gmail.com>
2016-07-26 11:06:35 -0400

    getting user preference functionality added

--- app/scripts/data.js
+++ app/scripts/data.js
@@ -1,7 +1,33 @@
 var questionsData = [{
-    questionText: 'Question 1',
+    questionText: 'Do ye like yer drinks strong?',
     correspondingIngredient: 'strong'
 }, {
-    questionText: 'Question 2',
+    questionText: 'Do ye like it with a salty tang?',
     correspondingIngredient: 'salty'
+}, {
+    questionText: 'Are ye a lubber who likes it bitter?',
+    correspondingIngredient: 'bitter'
+}, {
+    questionText: 'Would ye like a bit of sweetness with yer poison?',
+    correspondingIngredient: 'sweet'
+}, {
+    questionText: 'Are ye one for a fruity finish?',
+    correspondingIngredient: 'fruity'
+}];
+
+var ingredientsData = [{
+    typeOfIngredient: 'strong',
+    listOfIngredients: ['glug of rum', 'slug of whisky', 'splash of gin']
+}, {
+    typeOfIngredient: 'salty',
+    listOfIngredients: ['olive on a stick', 'salt-dusted rim', 'rasher of bacon']
+}, {
+    typeOfIngredient: 'bitter',
+    listOfIngredients: ['shake of bitters', 'splash of tonic', 'twist of lemon peel']
+}, {
+    typeOfIngredient: 'sweet',
+    listOfIngredients: ['sugar cube', 'spoonful of honey', 'splash of cola']
+}, {
+    typeOfIngredient: 'fruity',
+    listOfIngredients: ['slice of orange', 'dash of cassis', 'cherry on top']
 }];
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,19 +1,32 @@
-$(function () {
-
+$(function() {
     /* Questions constructor */
     var Question = function(questionText, correspondingIngredient) {
         var self = this;
-
         self.questionText = questionText;
         self.correspondingIngredient = correspondingIngredient;
+        self.answer = ko.observable('false');
     };
 
     /* Ingredient constructor */
     var Ingredient = function(typeOfIngredient, listOfIngredients) {
         var self = this;
-
         self.typeOfIngredient = typeOfIngredient;
-        self.listOfIngredients = listofIngredients;
+        self.listOfIngredients = listOfIngredients;
+    };
+
+    Ingredient.prototype.formattedList = function() {
+        return this.listOfIngredients.join(', ');
+    };
+
+    /* Preferences constructor */
+    var Preferences = function() {
+        var self = this;
+        self.preferenceList = ko.observableArray([]);
+        self.test = "test";
+    };
+
+    Preferences.prototype.addPreference = function(preference) {
+        this.preferenceList.push(preference);
     };
 
     /* Pantry constructor */
@@ -24,27 +37,41 @@ $(function () {
     /* Bartender constructor */
     var Bartender = function() {
         var self = this;
-
-        self.createDrink = function() {
-
-        };
     };
+
+    Bartender.prototype.createDrink = function() {};
 
     var BarViewModel = function() {
         var self = this;
 
-        self.questions = [];
-        self.ingredients = [];
+        self.currentCounter = ko.observable(0);
 
-        self.initializeQuestions = function(questions) {
-            ko.utils.arrayForEach(questions, function(question) {
-                self.questions.push(new Question(question.questionText, question.correspondingIngredient));
-            });
+        self.questions = ko.observableArray(ko.utils.arrayMap(questionsData, function(question) {
+            return new Question(question.questionText, question.correspondingIngredient);
+        }));
+
+        self.currentQuestion = ko.observable(self.questions()[self.currentCounter()]);
+
+        self.ingredients = ko.observableArray(ko.utils.arrayMap(ingredientsData, function(ingredient) {
+            return new Ingredient(ingredient.typeOfIngredient, ingredient.listOfIngredients);
+        }));
+
+        self.preferences = new Preferences();
+
+        self.handleClick = function(data, event) {
+            if (data.currentQuestion().answer() === 'true') {
+                self.preferences.addPreference(data.currentQuestion().correspondingIngredient);
+            }
+
+            if (self.currentCounter() < self.questions().length) {
+                self.currentCounter(self.currentCounter() + 1);
+
+            }
+            if (self.currentCounter() != self.questions().length) {
+                self.currentQuestion(self.questions()[self.currentCounter()]);
+            }
+
         };
-
-        self.initializeQuestions(questionsData);
-
-
     };
 
     ko.applyBindings(new BarViewModel());
